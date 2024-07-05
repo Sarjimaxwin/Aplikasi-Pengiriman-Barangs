b491658733c5c9edac8c616dcec19570168932c4
Mohamad Bintang Prasetyo <89068958+bintang123456@users.noreply.github.com>
2024-07-05 21:16:43 +0700

    nutup </a>.php

--- dashboard.php
+++ dashboard.php
@@ -49,7 +49,7 @@
                     <div class="row">
                         <div class="col-sm-6 col-md-3">
                             <div class="info-box"> <span class="info-box-icon text-bg-warning shadow-sm"> <i class="bi bi-cart"></i></span>
-                                <div class="info-box-content"> <a href="pesanan.php" class="link-dark text-center">
+                                <div class="info-box-content"> <a href="pesanan.php" class="link-dark text-center"> </a>
                                     <span class="info-box-text"> Total Pengiriman</span> <span class="info-box-number">
                                     <?php echo $totalPesanan; ?></span>
                                                   
@@ -60,12 +60,10 @@
                 </div>          
             </div>
     </main>
-    <footer class="app-footer">
-      <?php
-      include("footer.php")
-      ?>
-    </footer>
-   
+    <?php
+    include("footer.php");
+    
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/browser/overlayscrollbars.browser.es6.min.js" integrity="sha256-H2VM7BKda+v2Z4+DRy69uknwxjyDRhszjXFhsL4gD3w=" crossorigin="anonymous"></script> <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha256-whL0tQWoY1Ku1iskqPFvmZ+CHsvmRWx/PIoEvIeWh4I=" crossorigin="anonymous"></script> <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha256-YMa+wAM6QkVyz999odX7lPRxkoYAan8suedu4k2Zur8=" crossorigin="anonymous"></script> <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
