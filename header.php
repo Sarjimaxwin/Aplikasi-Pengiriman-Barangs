91216c577aeb12471eaac57d367da54d77dfdd20
Mohamad Bintang Prasetyo <89068958+bintang123456@users.noreply.github.com>
2024-07-08 09:20:42 +0700

    Update header.php

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ if(empty($_SESSION['email'])){
                   
                    
                     
-                    <li class="nav-item dropdown user-menu"> <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"> <img src="assets/img/user2-160x160.jpg" class="user-image rounded-circle shadow" alt="User Image"> <span class="d-none d-md-inline"><?php echo( $_SESSION['nama'] ); ?> </span> </a> 
+                    <li class="nav-item dropdown user-menu"> <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"> <span class="d-none d-md-inline"><?php echo( $_SESSION['nama'] ); ?> </span> </a> 
 
                         <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <!--begin::User Image-->
                             <li class="user-header text-bg-primary"> <img src="assets/img/avatar5.png" class="rounded-circle shadow" alt="User Image">
@@ -22,7 +22,7 @@ if(empty($_SESSION['email'])){
                                 <?php echo( $_SESSION['nama'] ); ?>
                                 </p>
                             </li> <!--end::User Image--> <!--begin::Menu Body-->
-                            <li class="user-footer"> <a href="profil.php" class="btn btn-default btn-flat">Profile</a> <a href="signout.php" class="btn btn-default btn-flat float-end">Sign out</a> </li> <!--end::Menu Footer-->
+                            <li class="user-footer"> <a href="profil.php" class="btn btn-default btn-flat">Profil</a> <a href="signout.php" class="btn btn-default btn-flat float-end">Keluar</a> </li> <!--end::Menu Footer-->
                         </ul>
                     </li> <!--end::User Menu Dropdown-->
                 </ul> <!--end::End Navbar Links-->
