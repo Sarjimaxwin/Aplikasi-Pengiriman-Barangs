2a9191aece981d1d5d963cf9339cd1a9102c76b3
Mohamad Bintang Prasetyo <89068958+bintang123456@users.noreply.github.com>
2024-07-10 19:35:14 +0700

    ubah warna footer.php

--- footer.php
+++ footer.php
@@ -1,18 +1,20 @@
 <?php
-
-if(empty($_SESSION['email'])){
-    echo"<script>alert('Anda tidak dapat mengakses halaman ini.');
+if (empty($_SESSION['email'])) {
+    echo "<script>alert('Anda tidak dapat mengakses halaman ini.');
     window.location.href = 'index.php';</script>";
     exit();
 }
-    ?>
+?>
 
-<footer class="app-footer"> <!--begin::To the end-->
-            <!--end::To the end--> <!--begin::Copyright-->
-             
-               <b> Copyright &copy; 2024 
-                <a  class="text-decoration-none">Anto Tegar Bintang</a>.
-        
-            All rights reserved.
-            <!--end::Copyright-->
-        </footer> <!--end::Footer-->
+<footer class="app-footer" style="background-color: red;">
+    <!--begin::To the end-->
+    <!--end::To the end-->
+    <!--begin::Copyright-->
+    <b>
+        <span style="color: white;">Copyright &copy; 2024 </span>
+        <a href="#" class="text-decoration-none" style="color: white;">Anto Tegar Bintang</a>.
+    </b>
+    All rights reserved.
+    <!--end::Copyright-->
+</footer>
+<!--end::Footer-->
