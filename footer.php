e66f988500819d03dcfa8b745b6f37c5b3f325f4
Tegarrrrr03 <tegartugas6@gmail.com>
2024-07-17 07:42:30 +0700

    Ubahfooter.php

--- footer.php
+++ footer.php
@@ -17,17 +17,17 @@
             background-image: linear-gradient(to right, #ff3366, #ff9933);
             color: white;
             text-align: left;
-            padding: 20px 0;
+            padding: 10px 20px; /* Adjusted padding to make footer shorter */
             position: fixed;
             bottom: 0;
             width: 100%;
         }
         .social-icons {
-            margin-bottom: 10px;
+            margin-bottom: 5px; /* Adjusted margin bottom */
         }
         .social-icons a {
             color: white;
-            margin: 0 10px;
+            margin-right: 10px;
             font-size: 20px;
             display: inline-block;
             transition: transform 0.3s ease;
@@ -38,7 +38,12 @@
         .social-icons .icon-label {
             display: block;
             font-size: 12px;
-            margin-top: 5px;
+            margin-top: 2px; /* Adjusted margin top */
+        }
+        b {
+            display: inline-block;
+            vertical-align: middle;
+            margin-right: 5px;
         }
     </style>
 </head>
@@ -58,10 +63,8 @@
                 <span class="icon-label">Bintang</span>
             </a>
         </div>
-        <b>
-            <span>&copy; 2024 </span>
-            <a href="#" class="text-decoration-none">Anto Tegar Bintang</a>.
-        </b>
+        <b>&copy; 2024 </b>
+        <a href="#" class="text-decoration-none">Anto Tegar Bintang</a>.
         All rights reserved.
     </footer>
     <!-- Your other HTML content here -->
